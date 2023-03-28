b16494c00cacc3f7a8a19eba7157ff763a135586
wildryry <77308934+wildryry@users.noreply.github.com>
2023-03-28 13:43:47 -0700

    Update poo.py

--- python/poo.py
+++ python/poo.py
@@ -2,6 +2,8 @@ import pygame
 
 import sys
 
+from random import randint
+
 pygame.init() 
 
 pygame.display.init()
@@ -9,25 +11,47 @@ pygame.display.init()
 ground = pygame.image.load('ground.png')
 sky = pygame.image.load('sky.png')
 
-def button_draw(button_list):
-    
-    for x in button_list:
-         print(x)
+button_list = []
+
+class Mouse(pygame.sprite.Sprite):
+	def __init__(self,pos_x,pos_y):
+		super().__init__()
+		self.image = pygame.image.load('mouse.png').convert_alpha()
+		self.rect = self.image.get_rect()
+
+def button_draw():
+	global button_list
+	
+	for x in button_list:
+		
+		
+		screen.blit(button_surface,x.x(randint(0,300)))
+		
+
 
 screen = pygame.display.set_mode((ground.get_width(),400))
 guy = pygame.image.load('mouse.png').convert_alpha()
 guy_rect = guy.get_rect(topleft = pygame.mouse.get_pos())
 
-button_list = []
+mouse = Mouse(300,200)
+
+mouse_group = pygame.sprite.Group()
+mouse_group.add(mouse)
+
 
 button_surface = pygame.image.load('boogie_button.png').convert_alpha()
-button_rect = button_surface.get_rect((300,200))
+button_rect = button_surface.get_rect(center = (300,200))
 
 back_ground_rect = pygame.Surface.get_rect(screen)
 
 done_not = True
 
 pygame.mouse.set_visible(False) 
+
+
+clock = pygame.time.Clock()  
+
+pygame.time.set_timer(pygame.USEREVENT+1, 1100)
 
 while done_not:
 
@@ -39,15 +63,25 @@ while done_not:
 			
 			print(pygame.key.get_pressed())
 	
-	guy_rect = guy.get_rect(center = pygame.mouse.get_pos())
+		if event.type == pygame.USEREVENT+1:
+			button_list.append(button_rect)
+			
+
+	
 
 	screen.blit(sky,(0,0))
 	screen.blit(ground ,(0,360))
-	screen.blit(guy ,guy_rect)
 	
-	button_draw()
+	
+	
+	#button_draw()
+	#mouse sprite things
+	mouse.pos_x = pygame.mouse.get_pos()
+	mouse_group.draw(screen)
 
 	pygame.display.update()
+	clock.tick(60)
+
 
 pygame.quit()
 sys.exit()	
