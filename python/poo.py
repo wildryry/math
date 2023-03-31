8fea0c001ce7e15e25e0d4fd70022123fa533120
wildryry <77308934+wildryry@users.noreply.github.com>
2023-03-31 13:43:15 -0700

    Update poo.py

--- python/poo.py
+++ python/poo.py
@@ -11,14 +11,16 @@ pygame.display.init()
 ground = pygame.image.load('images/ground.png')
 sky = pygame.image.load('images/sky.png')
 
-button_list = []
+
 
 class Mouse(pygame.sprite.Sprite):
 	def __init__(self,pos_x,pos_y):
 		super().__init__()
 		self.image = pygame.image.load('images/mouse.png').convert_alpha()
 		self.rect = self.image.get_rect()
-
+		pos_x = self.rect.x
+		pos_y = self.rect.y
+	
 	def update(self):
 		self.rect.center = pygame.mouse.get_pos()
 
@@ -30,41 +32,31 @@ class Button(pygame.sprite.Sprite):
 		self.time = time
 	
 	def update(self):
+		if button.rect.colliderect(mouse.rect) and pygame.mouse.get_pressed([0]) == True:
+			button_group.remove(button)
+		print(pygame.mouse.get_pressed([0]))
 		
-		
-		print(self.time * int(pygame.time.get_ticks))
 
-def button_draw():
-	global button_list
-	
-	for x in button_list:
-		
-		
-		screen.blit(button_surface,x.x(randint(0,300)))
 		
 
 
 screen = pygame.display.set_mode((ground.get_width(),400))
-guy = pygame.image.load('images/mouse.png').convert_alpha()
-guy_rect = guy.get_rect(topleft = pygame.mouse.get_pos())
+
 
 #Button
 button = Button(10000,0,0)
 button_group = pygame.sprite.Group()
-button_group.add(button)
+
 
 #mouse
 mouse = Mouse(300,200)
 mouse_group = pygame.sprite.Group()
+mouse_group.add(mouse)
 
-
-
-button_surface = pygame.image.load('images/boogie_button.png').convert_alpha()
-button_rect = button_surface.get_rect(center = (300,200))
 
 back_ground_rect = pygame.Surface.get_rect(screen)
 
-done_not = True
+done = False
 
 pygame.mouse.set_visible(False) 
 
@@ -73,28 +65,36 @@ clock = pygame.time.Clock()
 
 pygame.time.set_timer(pygame.USEREVENT+1, 1100)
 
-while done_not:
+
+for x in range(100):
+
+	button.rect.y = randint(10,390)
+	button.rect.x = randint(0,600)
+	button_group.add(button)
+
+while not done:
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
-			done_not = False
+			done = True
 
 		if event.type == pygame.KEYDOWN:
 			
 			print(pygame.key.get_pressed())
 	
 		if event.type == pygame.USEREVENT+1:
-			button_list.append(button_rect)
-			
+			print('event')
+			button_group.add(button)
 
-	button_group.update()
+	
 
 	screen.blit(sky,(0,0))
 	screen.blit(ground ,(0,360))
 	
+	button_group.update()
+	button_group.draw(screen)
 	
 	
-	#button_draw()
 	#mouse sprite things
 	
 	mouse_group.update()
