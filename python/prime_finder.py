38a4dc2526cae5b2281be50598f1e6bcd0ce8a55
wildryry <77308934+wildryry@users.noreply.github.com>
2023-04-20 14:11:52 -0700

    poo peep oepoeee

--- python/prime_finder.py
+++ python/prime_finder.py
@@ -2,38 +2,20 @@ import sys
 
 
 def find_prime(the_number):
-    primes300 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293]
-    onetimething=True
-    the_checker = int(2)
-    while the_checker < the_number:
+    if the_number <= 1:
+        return False
         
-
-        if the_checker > 62 and onetimething == True:
-            the_checker += 231
-            onetimething = False
-
-        if onetimething == True:
-            
-            if primes300[the_checker - 1] % the_checker!= 0 :
-                
-                the_checker += 1
-                if the_checker == the_number:
-                    return  True
-                
-            else: 
-                return False
-                break 
-
+    the_checker = 2
+    while the_checker * the_checker <= the_number:
+        if the_number % the_checker == 0:
+            return False
+        if the_checker > 2:
+            the_checker += 2
         else:
-            if the_number % the_checker != 0 :
-                
-                the_checker += 1
-                if the_checker == the_number:
-                    return  True
-                
-            else: 
-                return False
-                break
+            the_checker += 1
+
+    return True
+
         
 # varablies
 
@@ -77,4 +59,5 @@ while not_done:
     print(int(precent_done),'%')
     finder += 2     
 
-print(primes,'rock and stone')
+print(primes)
+print('prime_finder.py')
