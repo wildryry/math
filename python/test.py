e68a86e29327dafec93f0071c8fd06c998fa5a0c
wildryry <77308934+wildryry@users.noreply.github.com>
2023-04-21 13:42:32 -0700

    stuff done

--- python/test.py
+++ python/test.py
@@ -1,5 +1,5 @@
 import pygame
-
+import sys
 # Set up Pygame
 pygame.init()
 size = (800, 600)
@@ -10,10 +10,10 @@ pygame.display.set_caption("Linear Line Graph")
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (24, 135, 7)
-
+BLUE = (43, 94, 214)
 # Define the equation of the line (y = mx + b)
 m = 2
-b = 50
+b = 0
 
 # Define the x and y values to plot
 x_values = range(0, 800)
@@ -28,17 +28,24 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
 
-    # Clear the screen
+    
+    
+
+    # Clear the screen test.py
     screen.fill(BLACK)
 
     # Draw the line
-    pygame.draw.aaline(screen, GREEN, [0, b], [800, int(m * 800 + b)], 5)
-
+    pygame.draw.aaline(screen, GREEN, [400, (-1*b + 300)], [800, int(-1*m * 800 + b)], 5)
+    pygame.draw.aaline(screen, GREEN, [400, (-1*b + 300)], [800, int(*m * 800 + b)], 5)
+    
     # Update the screen
     pygame.display.flip()
+    
+    
 
     # Wait for a short period of time to control the frame rate
     clock.tick(60)
 
 # Quit Pygame
 pygame.quit()
+sys.exit()
