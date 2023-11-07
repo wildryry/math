a1291306c72df265c644b5a17a6657a98ca19645
wildryry <77308934+wildryry@users.noreply.github.com>
2023-11-07 09:54:55 -0800

    update

--- python/math/morsetext.py
+++ python/math/morsetext.py
@@ -4,13 +4,20 @@ output_string = 0
 
 if input('text to morse (y/n):').lower() == "y":
     input_string = input('input string :')
-    morse_code_data = open('morse_code_chart.csv')
+    morse_code_data = open('morse_code_chart.csv', "r", newline='')
     reader = csv.reader(morse_code_data)
     data = list(reader)
-    morse_code_data.close()
+    #morse_code_data.close()
 
-    for letter in input_string:
-        print('bals')
+
+    y_index = 0
+    for x_index,letter in enumerate(input_string):
+        #if x_index >26:
+        #    y_index += 2
+        #    x_index += -26
+        
+        
+        
 
         None
 input('morse to text (y/n):')
