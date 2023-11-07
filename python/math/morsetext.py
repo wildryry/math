03d98ae48496a67677e76f0b42fa578110652f91
wildryry <77308934+wildryry@users.noreply.github.com>
2023-11-07 14:02:57 -0800

    Update morsetext.py

--- python/math/morsetext.py
+++ python/math/morsetext.py
@@ -1,6 +1,6 @@
 import csv
 
-output_string = 0
+output_string = ""
 
 if input('text to morse (y/n):').lower() == "y":
     input_string = input('input string :')
@@ -9,16 +9,51 @@ if input('text to morse (y/n):').lower() == "y":
     data = list(reader)
     #morse_code_data.close()
 
+    
+    for letter in input_string:
+        x_index = 0
+        y_index = 0
+        for index,cell in enumerate(range(36)):
+            if x_index > 25:
+                x_index = 0
+                y_index = 2
 
-    y_index = 0
-    for x_index,letter in enumerate(input_string):
-        #if x_index >26:
-        #    y_index += 2
-        #    x_index += -26
-        
-        
-        
+            
+            if data[y_index+1][x_index] == letter:
 
-        None
-input('morse to text (y/n):')
-input('theres nothing else (y/n):')
+                output_string += data[y_index][x_index] + " "
+        
+        
+            x_index += 1
+    morse_code_data.close()
+            
+print(output_string)
+if input('morse to text (y/n):').lower() == "y":
+    input_string = input('input string :')
+    morse_code_data = open('morse_code_chart.csv', "r", newline='')
+    reader = csv.reader(morse_code_data)
+    data = list(reader)
+    #morse_code_data.close()
+
+    
+    for letter in input_string:
+        x_index = 0
+        y_index = 1
+        for index,cell in enumerate(range(36)):
+            if x_index > 25:
+                x_index = 0
+                y_index = 2
+
+            
+            if data[y_index-1][x_index] == letter:
+
+                output_string += data[y_index][x_index] + " "
+        
+        
+            x_index += 1
+    morse_code_data.close()
+            
+print(output_string)
+
+
+#input('theres nothing else (y/n):')
