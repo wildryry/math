138f22afe3fb1e3bc1ef33f6efb1a8731f7c4537
wildryry <77308934+wildryry@users.noreply.github.com>
2024-11-05 15:10:13 -0800

    update

---
+++ HTML/Javascript_1.js
@@ -0,0 +1,17 @@
+function helloWorld() {
+    console.log("hello world")
+    const button = document.getElementById("pageButton")
+    const para = document.getElementById("output")
+    //button.setAttribute("value","your a gay")
+    /*
+    for (i=0; i < 1; i++) {
+        para.innerHTML += "1"
+    }
+    */
+   
+
+}
+function reset() {
+    const para = document.getElementById("output")
+    para.innerHTML =""
+}
