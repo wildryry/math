49058ef9e808e5cb187d26d5c2700fba43a114a4
wildryry <77308934+wildryry@users.noreply.github.com>
2024-11-06 14:55:02 -0800

    update

--- HTML/Javascript_1.js
+++ HTML/Javascript_1.js
@@ -1,17 +1,33 @@
+
 function helloWorld() {
+    
     console.log("hello world")
     const button = document.getElementById("pageButton")
     const para = document.getElementById("output")
+    const para2 = document.getElementById("output2")
+    const para3 = document.getElementById("output3")
     //button.setAttribute("value","your a gay")
-    /*
-    for (i=0; i < 1; i++) {
-        para.innerHTML += "1"
+    
+    if (para2.innerHTML == "////////////////////////////////////////////////////////////////////////////////////////////////////") {
+        para2.innerHTML = ""
+        
+        para3.innerHTML = para.innerHTML
+        
+        para.innerHTML = "0"
+
     }
-    */
+    
+    for (i=0; i < 1; i++) {
+        para.innerHTML = Math.floor(Math.random() * 9) + parseInt(para.innerHTML)
+        para2.innerHTML += "/"
+    }
+    
+    
    
 
 }
 function reset() {
     const para = document.getElementById("output")
-    para.innerHTML =""
+    para.innerHTML ="1"
+    para2.innerHTML ="0"
 }
